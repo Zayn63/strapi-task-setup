cf5856ed897971dc28995edde4dd377a647d0d23
Zayn63 <icyflamemansuri63@gmail.com>
2025-07-23 10:22:37 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,54 +1,22 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  region = "eu-north-1"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
+resource "aws_instance" "strapi" {
+  ami                    = "ami-0fe8bec493a81c7da"
+  instance_type          = "t3.micro"
+  key_name               = "zayn-key"
+  vpc_security_group_ids = ["sg-0e0fc6d36b1f4d4ae"]
 
-resource "aws_subnet" "subnet" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-}
-
-resource "aws_security_group" "strapi_sg" {
-  name        = "strapi-sg"
-  description = "Allow ports 22 and 80"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "strapi_ec2" {
-  ami                         = "ami-0c02fb55956c7d316" # Ubuntu Server 22.04 LTS
-  instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.subnet.id
-  security_groups             = [aws_security_group.strapi_sg.name]
-  associate_public_ip_address = true
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install -y docker.io
+              systemctl start docker
+              docker run -d -p 80:1337 --name strapi ${var.docker_image}
+              EOF
 
   tags = {
-    Name = "StrapiServer"
+    Name = "Strapi-Deployed-Instance"
   }
 }
